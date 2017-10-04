22482db5cf4319eddfc4152b55e9d80192cd93d7
Kris Sankaran <sankaran.kris@gmail.com>
2017-10-04 11:00:12 -0700

    split simulation and data analysis for prob cca

--- dimension_red/prob_cca.R
+++ dimension_red/prob_cca.R
@@ -2,10 +2,10 @@
 
 ## File description -------------------------------------------------------------
 ##
-## A study of using probabilistic CCA on simulated and real data.
+## Applying probabilistic CCA to real data.
 ##
 ## author: sankaran.kris@gmail.com
-## date: 09/28/2017
+## date: 10/04/2017
 
 ###############################################################################
 ## Libraries and setup
@@ -36,84 +36,9 @@ theme_update(
   legend.key = element_blank()
 )
 
-matnorm <- function(n, p, mu = 0, sigma = 1) {
-  matrix(
-    rnorm(n * p, mu, sigma),
-    n, p
-  )
-}
-
-#' Average across first dimension
-slice_mean <- function(x) {
-  apply(x, c(2, 3), mean)
-}
-
-#' Means from posterior samples
-parameter_means <- function(theta_samples) {
-  theta_hat <- list()
-  for (i in seq_along(theta_samples)) {
-    if (length(dim(theta_samples[[i]])) > 2) {
-      theta_hat[[i]] <- slice_mean(theta_samples[[i]])
-    } else {
-      theta_hat[[i]] <- mean(theta_samples[[i]])
-    }
-  }
-
-  names(theta_hat) <- names(theta_samples)
-  theta_hat
-}
-
-###############################################################################
-## Simulation study to validate approach
-###############################################################################
-K <- 3
-ps <- c(20, 50)
-n <- 100
-
-## simulate data
-theta <- list(
-  "Wx" = matnorm(20, K, 0, 2),
-  "Wy" = matnorm(50, K, 0, 2),
-  "Bx" = matnorm(20, K),
-  "By" = matnorm(50, K),
-  "xi_s" = matnorm(n, K),
-  "xi_x" = matnorm(n, K),
-  "xi_y" = matnorm(n, K)
-)
-
-x <- theta$xi_s %*% t(theta$Wx) +
-  theta$xi_x %*% t(theta$Bx) +
-  matnorm(n, ps[1], 0, 0.1)
-y <- theta$xi_s %*% t(theta$Wy) +
-  theta$xi_y %*% t(theta$By) +
-  matnorm(n, ps[2], 0, 0.1)
-
-## fit model
-m <- stan_model("prob_cca.stan")
-stan_data <- list(
-  "K" = K,
-  "p1" = ps[1],
-  "p2" = ps[2],
-  "n" = n,
-  "tau" = 5,
-  "x" = x,
-  "y" = y,
-  "id_x" = diag(ps[1]),
-  "id_y" = diag(ps[2]),
-  "id_k" = diag(K),
-  "zeros_k" = rep(0, K)
-)
-
-fit <- vb(m, stan_data, iter = 5000)
-theta_samples <- rstan::extract(fit)
-rm(fit)
-theta_hat <- parameter_means(theta_samples)
-plot(theta_hat$xi_s[, 1], theta$xi_s[, 2], asp = 1)
-
 ###############################################################################
 ## Real data application
 ###############################################################################
-
 ## read and prepare data
 raw <- read_data()
 processed <- process_data(raw$seqtab, raw$bc, raw$taxa)
@@ -132,6 +57,7 @@ stan_data <- list(
   "zeros_k" = rep(0, K)
 )
 
+m <- stan_model("prob_cca.stan")
 micro_fit <- vb(m, stan_data, iter = 100)
 micro_samples <- rstan::extract(micro_fit)
 rm(micro_fit)
