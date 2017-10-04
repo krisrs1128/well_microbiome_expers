2db4929b83c0a8a102b3ad17e38689fcc10153a6
Kris Sankaran <sankaran.kris@gmail.com>
2017-10-04 11:00:48 -0700

    real data application for lda / cca

--- dimension_red/lda_cca.R
+++ dimension_red/lda_cca.R
@@ -12,6 +12,7 @@
 ## Libraries and setup
 ###############################################################################
 library("phyloseq")
+library("rstan")
 library("tidyverse")
 library("reshape2")
 library("ggrepel")
@@ -19,6 +20,9 @@ library("viridis")
 library("vegan")
 source("prep_tables.R")
 source("plot.R")
+
+rstan_options(auto_write = TRUE)
+options(mc.cores = parallel::detectCores())
 
 ## cleaner ggplot theme
 scale_colour_discrete <- function(...)
@@ -51,3 +55,30 @@ processed <- process_data(raw$seqtab, raw$bc, raw$taxa, opts)
 ## Run and plot LDA / CCA on the two (scaled) tables
 ###############################################################################
 bc_mat <- scale(processed$bc)
+
+K <- 2
+L1 <- 3
+L2 <- 3
+
+stan_data <- list(
+  "n" = nrow(bc_mat),
+  "p1" = ncol(processed$x_seq),
+  "p2" = ncol(bc_mat),
+  "K" = K,
+  "L1" = L1,
+  "L2" = L2,
+  "sigma" = 1,
+  "tau" = 8,
+  "x" = processed$x_seq,
+  "y" = bc_mat,
+  "id_y" = diag(ncol(bc_mat)),
+  "id_k" = diag(K),
+  "id_l1" = diag(L1),
+  "id_l2" = diag(L2),
+  "zeros_k" = rep(0, K),
+  "zeros_l1" = rep(0, L1),
+  "zeros_l2" = rep(0, L2)
+)
+
+m <- stan_model("lda_cca.stan")
+stan_fit <- vb(m, data = stan_data)
