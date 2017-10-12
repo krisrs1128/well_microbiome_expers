1d6945fb863b9e42ee580b4eb67913253100e2d4
Kris Sankaran <sankaran.kris@gmail.com>
2017-10-12 16:34:00 -0700

    visualize spls coefficients similarly to multitask lasso

--- supervised/spls.R
+++ supervised/spls.R
@@ -47,7 +47,7 @@ theme_update(
 ## read and prepare the data
 ###############################################################################
 raw <- read_data()
-opts <- list("filt_k" = 0.5)
+opts <- list("filt_k" = 0.07)
 processed <- process_data(raw$seqtab, raw$bc, raw$taxa, opts)
 
 y <- scale(processed$bc)
@@ -56,8 +56,57 @@ cv_eval <- cv.spls(x, y, K = 1:6, eta = seq(0, 0.9, 0.05), scale.x = FALSE, fold
 cv_eval
 
 train_ix <- sample(1:nrow(x), 80)
-fit <- spls(x[train_ix, ], y[train_ix, ], cv_eval$K.opt, cv_eval$eta.opt)
+#fit <- spls(x[train_ix, ], y[train_ix, ], cv_eval$K.opt, cv_eval$eta.opt)
+fit <- spls(x[train_ix, ], y[train_ix, ], 2, 0.8)
 y_hat <- x %*% fit$betahat
 plot(y[train_ix, 24], y_hat[train_ix, 24])
 points(y[-train_ix, 24], y_hat[-train_ix, 24], col = "blue")
 abline(a = 0, b = 1, col = "red")
+
+
+###############################################################################
+## plot fitted coefficients
+###############################################################################
+seq_families <- processed$mseqtab %>%
+  select(seq_num, family) %>%
+  unique()
+
+site_ordered <- c(
+  "aoi", "age", "height_dxa", "weight_dxa",
+  "bmi", "Android_FM", "Android_LM", "Gynoid_FM", "Gynoid_LM", "L_Trunk_FM",
+  "L_Trunk_LM", "R_Trunk_FM", "R_Trunk_LM", "Trunk_FM", "Trunk_LM",
+  "L_Total_FM", "L_Total_LM", "R_Total_FM", "R_Total_LM", "Total_FM",
+  "Total_LM", "L_Leg_FM", "L_Leg_LM", "R_Leg_FM", "R_Leg_LM", "Legs_FM",
+  "Legs_LM", "L_Arm_FM", "L_Arm_LM", "R_Arm_FM", "R_Arm_LM", "Arms_FM",
+  "Arms_LM"
+)
+mass_type_ordered <- c(
+  site_ordered[!grepl("FM|LM", site_ordered)],
+  site_ordered[grepl("FM", site_ordered)],
+  site_ordered[grepl("LM", site_ordered)]
+)
+
+mbeta <- fit$betahat %>%
+  melt(varnames = c("seq_num", "feature")) %>%
+  left_join(seq_families) %>%
+  mutate(feature = factor(feature, mass_type_ordered))
+
+ggplot(mbeta) +
+  geom_tile(
+    aes(x = seq_num, y = feature, fill = value)
+  ) +
+  scale_fill_gradient2(
+    guide = guide_colorbar(ticks = FALSE, keyheight = 0.5),
+    low = "#40004b",
+    high = "#00441b"
+  ) +
+  scale_x_discrete(expand = c(0, 0)) +
+  scale_y_discrete(expand = c(0, 0)) +
+  facet_grid(. ~ family, scale = "free", space = "free") +
+  theme(
+    axis.text = element_blank(),
+    panel.spacing = unit(0, "cm"),
+    axis.text.y = element_text(size = 7, angle = 0, hjust = 0),
+    strip.text.x = element_text(size = 7, angle = 90, hjust = 0),
+    legend.position = "bottom"
+  )
