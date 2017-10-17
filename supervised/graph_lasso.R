04c4c5d7cd0a7f622de69d850fea8d424e4a9cab
Kris Sankaran <sankaran.kris@gmail.com>
2017-10-16 19:48:42 -0700

    plot results from graph lasso

--- supervised/graph_lasso.R
+++ supervised/graph_lasso.R
@@ -56,7 +56,62 @@ x <- scale(processed$x_seq)
 R <- cor(y)
 opts <- list(
   gamma = 0.01,
-  lambda = 0.01
+  lambda = 0.1,
+  eps = 0.01,
+  verbose = TRUE
 )
 
-gflasso(y, x, R)
+fit <- gflasso(y, x, R, opts)
+
+###############################################################################
+## plot fitted coefficients
+###############################################################################
+seq_families <- processed$mseqtab %>%
+  select(seq_num, family) %>%
+  unique()
+
+site_ordered <- c(
+  "aoi", "age", "height_dxa", "weight_dxa",
+  "bmi", "Android_FM", "Android_LM", "Gynoid_FM", "Gynoid_LM", "L_Trunk_FM",
+  "L_Trunk_LM", "R_Trunk_FM", "R_Trunk_LM", "Trunk_FM", "Trunk_LM",
+  "L_Total_FM", "L_Total_LM", "R_Total_FM", "R_Total_LM", "Total_FM",
+  "Total_LM", "L_Leg_FM", "L_Leg_LM", "R_Leg_FM", "R_Leg_LM", "Legs_FM",
+  "Legs_LM", "L_Arm_FM", "L_Arm_LM", "R_Arm_FM", "R_Arm_LM", "Arms_FM",
+  "Arms_LM"
+)
+mass_type_ordered <- c(
+  site_ordered[!grepl("FM|LM", site_ordered)],
+  site_ordered[grepl("FM", site_ordered)],
+  site_ordered[grepl("LM", site_ordered)]
+)
+
+species_order <- colnames(x)[hclust(dist(t(x)))$order]
+
+colnames(fit$B) <- colnames(y)
+mbeta <- fit$B %>%
+  melt(varnames = c("seq_num", "feature")) %>%
+  left_join(seq_families) %>%
+  mutate(
+    feature = factor(feature, mass_type_ordered),
+    seq_num = factor(seq_num, species_order)
+  )
+
+ggplot(mbeta) +
+  geom_tile(
+    aes(x = seq_num, y = feature, fill = value)
+  ) +
+  scale_fill_gradient2(
+    guide = guide_colorbar(ticks = FALSE, barheight = 0.6),
+    low = "#40004b",
+    high = "#00441b"
+  ) +
+  scale_x_discrete(expand = c(0, 0)) +
+  scale_y_discrete(expand = c(0, 0)) +
+  facet_grid(. ~ family, scale = "free", space = "free") +
+  theme(
+    axis.text = element_blank(),
+    panel.spacing = unit(0, "cm"),
+    axis.text.y = element_text(size = 5, angle = 0, hjust = 0),
+    strip.text.x = element_text(size = 7, angle = 90, hjust = 0),
+    legend.position = "bottom"
+  )
