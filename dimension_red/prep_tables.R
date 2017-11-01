e6fbe4881b146711b4d90dfc29cb24f925ea8e68
Kris Sankaran <sankaran.kris@gmail.com>
2017-11-01 12:53:22 -0700

    preprocessing documentation

--- dimension_red/prep_tables.R
+++ dimension_red/prep_tables.R
@@ -28,14 +28,31 @@ merge_default_opts <- function(opts = list()) {
   modifyList(default_opts, opts)
 }
 
+#' Read raw data
+#'
+#' Reads and applies basic preprocessing steps to the raw data.
+#'
+#' @param data_dir [string] The path to the raw data, relative to the current
+#'   working directory.
+#' @return results [list] A list containing the following essentially raw data
+#'   sets,
+#'    (1) taxa: The table giving taxonomic information for each sequence.
+#'    (2) seqtab: The raw counts table of species across samples.
+#'    (3) bc: The body composition data on the subset of people in the
+#'         microbiome study.
+#'    (4) bc_full: The body composition data across all participants in the
+#'         study.
 read_data <- function(data_dir = "../data/") {
   seqtab <- readRDS(file.path(data_dir, "seqtab.rds"))
+  batch <- read_csv(file.path(data_dir, "222_Participant_Batch_Numbers.csv"))
   bc <- readRDS(file.path(data_dir, "sample_data_bc.rds")) %>%
-    mutate(id = as.character(id))
+    mutate(id = as.character(id)) %>%
+    left_join(batch)
   colnames(bc) <- tolower(colnames(bc))
+
   colnames(seqtab) <- paste0("species_", seq_len(ntaxa(seqtab)))
   bc_full <- read_csv(file.path(data_dir, "WELL_China_1969_7.25.2017.csv")) %>%
-    rename(
+    dplyr::rename(
       gender = gender_it,
       age = age_it,
       height_dxa = height,
@@ -43,7 +60,9 @@ read_data <- function(data_dir = "../data/") {
     ) %>%
     mutate(
       aoi = android_fm / gynoid_fm,
-      id = as.character(id)
+      id = as.character(id),
+      operator = NA,
+      batch = NA
     ) %>%
     select_at(
       .vars = setdiff(tolower(colnames(bc)), "number")
@@ -53,13 +72,24 @@ read_data <- function(data_dir = "../data/") {
     data.frame() %>%
     rownames_to_column("seq")
   taxa$seq_num <- colnames(seqtab)
-  tree <- readRDS(file.path(data_dir, "phylo_tree.rds"))
-  list("taxa" = taxa, "seqtab" = seqtab, "bc" = bc, "bc_full" = bc_full, "tree" = tree)
+  list(
+    "taxa" = taxa,
+    "seqtab" = seqtab,
+    "bc" = bc,
+    "bc_full" = bc_full,
+    "tree" = readRDS(file.path(data_dir, "phylo_tree.rds"))
+  )
 }
 
 #' Prepare sample data
 #'
-#' reorder columns and convert to ratios.
+#' Reorders columns and converts to ratios.
+#'
+#' @param survey [data.frame] The raw body composition data frame.
+#' @param scale_sample_data [bool] If TRUE, numeric columns will be centered and
+#'   scaled (separately for each gender).
+#' @return The body composition data with log transformed diet and ratio
+#'   transformed lean and fat mass variables.
 prep_sample <- function(survey, scale_sample_data = FALSE) {
   sample <- data.frame(survey) %>%
     mutate(
@@ -69,7 +99,7 @@ prep_sample <- function(survey, scale_sample_data = FALSE) {
     select( ## reorder columns
       id, age, gender, height_dxa, weight_dxa,
       bmi, aoi, ends_with("_fm"), ends_with("_lm"),
-      starts_with("diet_")
+      starts_with("diet_"), batch, operator
     )%>%
     mutate_at( ## fm ratios
       vars(ends_with("fm"), -total_fm),
@@ -86,7 +116,9 @@ prep_sample <- function(survey, scale_sample_data = FALSE) {
     mutate( ## new variables
       fat_lean_ratio = total_fm / total_lm,
       total_fm = total_fm / (weight_dxa * 1000),
-      total_lm = total_lm / (weight_dxa * 1000)
+      total_lm = total_lm / (weight_dxa * 1000),
+      batch = as.factor(batch),
+      operator = as.factor(operator)
     )
 
   ## scale body measurements by gender
@@ -100,6 +132,12 @@ prep_sample <- function(survey, scale_sample_data = FALSE) {
   as.data.frame(sample)
 }
 
+#' Preprocess Taxa Table
+#'
+#' @param taxa [data.frame] The raw taxonomic data as a data.frame
+#' @return Transformed version of the taxonomic table, includign a new "family"
+#'   column with lumped factors, along with a "seq_num" column giving a concise
+#'   species name for each sequence.
 prepare_taxa <- function(taxa) {
   taxa <- taxa %>%
     mutate(
@@ -109,27 +147,78 @@ prepare_taxa <- function(taxa) {
     taxa$family,
     levels = names(sort(table(taxa$family), decreasing = TRUE))
   )
-  tax_cols <- colnames(taxa)
-  taxa <- tax_table(taxa)
-  colnames(taxa) <- tax_cols
+  taxa <- tax_table(as.matrix(taxa))
   taxa_names(taxa) <- taxa[, "seq_num"]
   taxa
 }
 
-
-#' scaling that ignores NAs
+#' Scaling that ignores NAs
+#'
+#' Scale a matrix ignoring any NA entries.
+#'
+#' @param x [matrix] The matrix to scale
+#' @return x [matrix] The matrix with centered and scaled columns, ignoring any
+#'   NA entries.
 safe_scale <- function(x) {
   x[!is.finite(x)] <- NA
   z <- x - mean(x, na.rm = TRUE)
   z / sd(x, na.rm = TRUE)
 }
 
-process_data <- function(seqtab, bc, bc_full, taxa, tree, opts = list()) {
-  opts <- merge_default_opts(opts)
+#' VST transform a phyloseq object
+vst_ps <- function(ps, sf_quantile = 0.95, ...) {
+  if (sample_data(ps)$gender[1] == "Female") {
+    fmla <- formula(~ gynoid_fm)
+  } else {
+    fmla <- formula(~ trunk_fm)
+  }
 
+  dds <- DESeqDataSetFromMatrix(
+    countData = t(get_taxa(ps)),
+    colData = sample_data(ps),
+    design = fmla
+  )
+
+  qs <- apply(counts(dds), 2, quantile, sf_quantile)
+  sizeFactors(dds) <- qs / exp(mean(log(qs)))
+  varianceStabilizingTransformation(dds, ...)
+}
+
+#' Preprocess Microbioem + Body Composition Data
+#'
+#' Prepare the output of read_data() for use in actual analysis. This basically
+#' wraps prepare_sample(), prepare_taxa() and some DESeq functions.
+#'
+#' @param seqtab [matrix] A matrix containing raw species counts across species.
+#' @param bc [data.frame] The body composition data on the subset of people in
+#'   the microbiome study.
+#' @param bc_full [data.frame] The full body composition data, across all
+#'   participants.
+#' @param taxa [data.frame] The original taxa table for the species in seqtab.
+#' @param tree [phylo] A phylogenetic tree relating species.
+#' @param opts [list] A list giving preprocessing options. Supported options
+#'   are,
+#'     - scale_sample_data: Should body composition data be centered and scaled?
+#'     - gender: If "Male" / "Female", only return male / female participants.
+#'        Otherwise returns all.
+#'     - filter_k: The proportion [in 0 to 1] of samples with more than a
+#'         samples, needed for filtering.
+#'     - filter_a: The threshold value used by the k over a filter.
+#'     - rlog: Should the data be rlog transformed?
+#'     - rlog_opts: A list giving options passed directly to rlog in DESeq2.
+#'     - outdir: To what directory should any intermediate rlog transformed data
+#'         be saved?
+#' @return resutls [list] A list containing preprocessed data. Includes the
+#'   following elements,
+#'     - ps [phyloseq]: A phyloseq object containing the merged otu, taxa, tree,
+#'        and sample data.
+#'     - bc_full [data.frame]: A data.frame containing body composition data for
+#'        all study participants, not just those with microbiome data.
+#'     - mseqtab [data.frame]: A "melted" version of the phyloseq object. Mainly
+#'        useful for annotated plots.
+process_data <- function(seqtab, bc, bc_full, taxa, tree, opts = list()) {
   ## preparing taxa and survey data
-  taxa <- prepare_taxa(taxa) %>%
-    filter(seq_num %in% colnames(seqtab))
+  taxa <- prepare_taxa(taxa)
   taxa_names(tree) <- taxa[, "seq_num"]
 
   bc_full <- prep_sample(bc_full, opts$scale_sample_data) %>%
@@ -142,25 +231,28 @@ process_data <- function(seqtab, bc, bc_full, taxa, tree, opts = list()) {
     sample_data(bc),
     taxa,
     phy_tree(tree)
-  ) %>%
-    subset_samples(gender == opts$gender) %>%
+  )
+
+  if (opts$gender == "Male") {
+    ps <- ps %>%
+      subset_samples(gender == "Male")
+  } else if (opts$gender == "Female") {
+    ps <- ps %>%
+      subset_samples(gender == "Female")
+  }
+
+  ps <- ps %>%
     filter_taxa(
       function(x) {
         mean(x > opts$filter_a) > opts$filter_k
       },
-      prune = TRUE
-    )
+      TRUE
+    ) %>%
+    transform_sample_counts(opts$transform_fun)
 
-  if (opts$rlog$rlog) {
-    fname <- paste0(as.character(opts), collapse = "")
-    dir.create("..", "data")
-    fpath <- file.path(opts$outdir, digest::digest(fname))
-    if (file.exists(fpath)) {
-      rlog_dds <- get(load(fpath))
-    } else {
-      rlog_dds <- do.call(rlog_ps, c(fpath, ps, opts$rlog))
-    }
-    otu_table(ps)@.Data <- t(assay(rlog_dds))
+  if (opts$vst) {
+    vst_dds <- do.call(vst_ps, c(ps, opts$vst_opts))
+    otu_table(ps)@.Data <- t(assay(vst_dds))
   }
 
   list(
@@ -170,6 +262,14 @@ process_data <- function(seqtab, bc, bc_full, taxa, tree, opts = list()) {
   )
 }
 
+#' Melt a Phyloseq
+#'
+#' Transform a phyloseq object to a data.frame
+#'
+#' @param ps [phyloseq] A phyloseq object with counts, sample data, and
+#'   taxonomic annotation to merge into a single data.frame
+#' @return mps [data.frame] The melted phyloseq object as a single data.frame,
+#'   with rows indeced by species x sample pairs.
 melt_ps <- function(ps) {
   get_taxa(ps) %>%
     melt(varnames = c("number", "seq_num")) %>%
