a2853ed1b52b507de97867d9717e57bf3d9a4ab9
Kris Sankaran <sankaran.kris@gmail.com>
2017-11-01 12:53:06 -0700

    use new preprocessing functions

--- supervised/spls.R
+++ supervised/spls.R
@@ -11,10 +11,11 @@
 ## Libraries and setup
 ###############################################################################
 library("phyloseq")
-library("tidyverse")
-library("reshape2")
-source("../dimension_red/prep_tables.R")
 library("spls")
+library("reshape2")
+library("tidyverse")
+source("../dimension_red/prep_tables.R")
+source("../dimension_red/plot.R")
 
 ## cleaner ggplot theme
 scale_colour_discrete <- function(...)
@@ -47,51 +48,51 @@ theme_update(
 ## read and prepare the data
 ###############################################################################
 raw <- read_data()
-opts <- list("filt_k" = 0.07, "filt_a" = 0)
-processed <- process_data(raw$seqtab, raw$bc, raw$taxa, opts)
+opts <- list(
+  "filter_k" = 0.07,
+  "filter_a" = 5,
+  "scale_sample_data" = TRUE,
+  "gender" = "Female",
+  "transform_fun" = identity,
+  "vst" = TRUE
+)
+processed <- process_data(
+  raw$seqtab,
+  raw$bc,
+  raw$bc_full,
+  raw$taxa,
+  raw$tree,
+  opts
+)
 
-y <- scale(processed$bc)
-x <- scale(processed$x_seq)
-cv_eval <- cv.spls(x, y, K = 1:6, eta = seq(0, 0.9, 0.05), scale.x = FALSE, fold = 5)
+y_df <- sample_data(processed$ps)
+keep_ix <- apply(y_df, 1, function(x) all(!is.na(x)))
+rm_cols <- which(colnames(y_df) %in% c("batch", "operator", "gender", "number", "id"))
+y <- scale(y_df[keep_ix, -rm_cols])
+x <- scale(get_taxa(processed$ps))
+cv_eval <- cv.spls(x, y, K = 4:8, eta = seq(0, 0.7, 0.05), scale.x = FALSE, fold = 5)
 cv_eval
 
-## train_ix <- sample(1:nrow(x), 80)
-#fit <- spls(x[train_ix, ], y[train_ix, ], cv_eval$K.opt, cv_eval$eta.opt)
-fit <- spls(x[train_ix, ], y[train_ix, ], 4, 0.7)
+train_ix <- sample(1:nrow(x), 80)
+fit <- spls(x[train_ix, ], y[train_ix, ], cv_eval$K.opt, cv_eval$eta.opt)
 y_hat <- x %*% fit$betahat
 plot(y[train_ix, 24], y_hat[train_ix, 24])
 points(y[-train_ix, 24], y_hat[-train_ix, 24], col = "blue")
 abline(a = 0, b = 1, col = "red")
 
 ## refit on full data
-fit <- spls(x, y, 4, 0.7)
+fit <- spls(x, y, cv_eval$K.opt, cv_eval$eta.opt)
 
 ###############################################################################
 ## plot fitted coefficients
 ###############################################################################
-seq_families <- processed$mseqtab %>%
-  select(seq_num, family) %>%
-  unique()
-
-site_ordered <- c(
-  "aoi", "age", "height_dxa", "weight_dxa",
-  "bmi", "Android_FM", "Android_LM", "Gynoid_FM", "Gynoid_LM", "L_Trunk_FM",
-  "L_Trunk_LM", "R_Trunk_FM", "R_Trunk_LM", "Trunk_FM", "Trunk_LM",
-  "L_Total_FM", "L_Total_LM", "R_Total_FM", "R_Total_LM", "Total_FM",
-  "Total_LM", "L_Leg_FM", "L_Leg_LM", "R_Leg_FM", "R_Leg_LM", "Legs_FM",
-  "Legs_LM", "L_Arm_FM", "L_Arm_LM", "R_Arm_FM", "R_Arm_LM", "Arms_FM",
-  "Arms_LM"
-)
-mass_type_ordered <- c(
-  site_ordered[!grepl("FM|LM", site_ordered)],
-  site_ordered[grepl("FM", site_ordered)],
-  site_ordered[grepl("LM", site_ordered)]
-)
+seq_fam <- seq_families(processed$mseqtab)
+mass_type_ordered <- mass_ordering()
 species_order <- colnames(x)[hclust(dist(fit$betahat))$order]
 
 mbeta <- fit$betahat %>%
   melt(varnames = c("seq_num", "feature")) %>%
-  left_join(seq_families) %>%
+  left_join(seq_fam) %>%
   mutate(
     feature = factor(feature, mass_type_ordered),
     seq_num = factor(seq_num, species_order)
@@ -100,6 +101,11 @@ mbeta <- fit$betahat %>%
 ggplot(mbeta) +
   geom_tile(
     aes(x = seq_num, y = feature, fill = value)
+  ) +
+  geom_rect(
+    aes(col = family),
+    fill = "transparent", size = 2,
+    xmin = -Inf, xmax = Inf, ymin = -Inf, ymax = Inf
   ) +
   scale_fill_gradient2(
     guide = guide_colorbar(ticks = FALSE, keyheight = 0.5),
@@ -113,7 +119,7 @@ ggplot(mbeta) +
     axis.text = element_blank(),
     panel.spacing = unit(0, "cm"),
     axis.text.y = element_text(size = 7, angle = 0, hjust = 0),
-    strip.text.x = element_text(size = 7, angle = 90, hjust = 0),
+    strip.text.x = element_blank(),
     legend.position = "bottom"
   )
 
@@ -125,28 +131,28 @@ ggsave(
 
 large_species <- mbeta %>%
   filter(
-    feature %in% c("Total_LM", "Total_FM")
+    feature %in% c("total_lm", "total_fm")
   ) %>%
   group_by(seq_num) %>%
   mutate(norm = sqrt(sum(value ^ 2))) %>%
-  filter(norm > 0.085) %>%
+  filter(norm > 0.1) %>%
   arrange(desc(norm))
 
 mlarge_species <- melt(
   data.frame(
     "Number" = rownames(x),
     x[, unique(large_species$seq_num)],
-    y[, c("Total_FM", "Total_LM")]
+    y[, c("total_fm", "total_lm")]
   ),
-  id.vars = c("Number", "Total_FM", "Total_LM"),
+  id.vars = c("Number", "total_fm", "total_lm"),
   variable.name = "seq_num"
 ) %>%
-  left_join(seq_families)
+  left_join(seq_fam)
 
 mlarge_species$seq_num <- factor(
   mlarge_species$seq_num,
   mbeta %>%
-    filter(feature == "Total_LM") %>%
+    filter(feature == "total_lm") %>%
     arrange(desc(value)) %>%
     .[["seq_num"]]
 )
@@ -155,7 +161,7 @@ ggplot(mlarge_species) +
   geom_hline(yintercept = 0, size = 0.1, alpha = 0.8) +
   geom_vline(xintercept = 0, size = 0.1, alpha = 0.8) +
   geom_point(
-    aes(x = value, y = Total_LM, col = family),
+    aes(x = value, y = total_lm, col = family),
     size = 0.7, alpha = 0.8
   ) +
   facet_wrap(~seq_num, ncol = 8) +
@@ -168,11 +174,11 @@ ggsave(
   height = 6.3
 )
 
-## same plot for Total FM
+## same plot for total fm
 mlarge_species$seq_num <- factor(
   mlarge_species$seq_num,
   mbeta %>%
-    filter(feature == "Total_FM") %>%
+    filter(feature == "total_fm") %>%
     arrange(desc(value)) %>%
     .[["seq_num"]]
 )
@@ -181,13 +187,14 @@ ggplot(mlarge_species) +
   geom_hline(yintercept = 0, size = 0.1, alpha = 0.8) +
   geom_vline(xintercept = 0, size = 0.1, alpha = 0.8) +
   geom_point(
-    aes(x = value, y = Total_FM, col = family),
+    aes(x = value, y = total_fm, col = family),
     size = 0.7, alpha = 0.8
   ) +
   facet_wrap(~seq_num, ncol = 8) +
   theme(
     legend.position = "bottom"
   )
+
 ggsave(
   "../chapter/figure/spls/total_fm_species.png",
   width = 7.4,
